554797430363459c5e3b515f11e4ccca0fd42dfb
Malte Blanke <malte.blanke@urs-os.de>
2023-05-04 12:49:21 +0200

    laravel-8-from-scratch

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\PostController;
+
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
+    ]);
+});
